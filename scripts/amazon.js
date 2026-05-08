72741d422f5f8215e36c35a32cf5a7325e63d207
arun-pandian001 <a8557213@gmail.com>
2026-05-08 20:13:11 +0530

    Update amazon.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -7,7 +7,7 @@ let lists='';
 products.forEach((product)=>{
 lists +=`  <div class="product-container">
           <div class="product-image-container">
-            <img class="product-image" src="${product.images}">
+            <img class="product-image" src="${product}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
