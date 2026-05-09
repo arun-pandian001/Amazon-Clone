a373b54bb6f0aec4a2e98cb09158838857b4292c
arun-pandian001 <a8557213@gmail.com>
2026-05-09 15:57:37 +0530

    deleting for file structure

--- data/deliveryoptiondata.js
+++
@@ -1,2 +0,0 @@
-export const Deliveryoption=[{id:'1', deliverydays:7,price:0},{id:'2' ,deliverydays:3 ,price:499},
-    {id:'3', deliverydays:1 ,price:599}]
--- scripts/checkout.js
+++
@@ -1,6 +0,0 @@
-import { rendersummaryorder } from "./checkout/rendersummaryorder.js";
-import { paymentsummary } from "./checkout/paymentsummary.js";
-import { cart } from "../data/cart.js";
-
-rendersummaryorder()
-if(cart.length>0){paymentsummary()}
